e796bb5c46dd6970ec49266234ed7c24ca7dd503
MAZTRO <design.mallet@gmail.com>
2020-04-01 17:19:56 +0000

    Fix init in basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -33,12 +33,12 @@ class BaseModel:
                 if key != "__class__":
                     setattr(self, key, value)
 
-            if "id" not is kwargs.keys():
+            """ if "id" not is kwargs.keys():
                 self.id = str(uuid.uuid4())
             if "created_at" not in kwargs.keys():
                 self.created_at = datetime.now()
             if "updated_at" not in kwargs.keys():
-                self.updated_at = datetime.now()
+                self.updated_at = datetime.now() """
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
