e20bae60d9f9cee4493673d76d2df05852160c62
MAZTRO <design.mallet@gmail.com>
2020-04-02 23:24:37 +0000

    Fix Pep8 in Place file

--- models/place.py
+++ models/place.py
@@ -50,14 +50,21 @@ class Place(BaseModel, Base):
     metadata = Base.metadata
 
     place_amenity = Table('place_amenity', metadata,
-        Column('place_id', String(60), ForeignKey("places.id"), primary_key=True, nullable=False),
-        Column('amenity_id', String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False)
-    )
+                          Column('place_id', String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id',
+                                 String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True,
+                                 nullable=False))
 
-    if os.getenv("HBNB_TYPE_STORAGE") ==  "db":
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship('Review', cascade='all, delete',
                                backref='place')
-        amenities = relationship('Amenity', secondary=place_amenity, viewonly=False, back_populates='place_amenities')
+        amenities = relationship('Amenity', secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates='place_amenities')
     else:
         @property
         def reviews(self):
