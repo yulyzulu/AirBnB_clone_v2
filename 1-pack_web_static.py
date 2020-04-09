636025b8f17276fa8a1ebd29748834d3c4983f12
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-04-09 18:11:21 +0000

    pep8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,15 +5,18 @@ from fabric.decorators import runs_once
 from datetime import datetime
 from os.path import getsize
 
+
 @runs_once
 def do_pack():
     local("mkdir -p versions")
     date_time = datetime.now().strftime("%Y%m%d%H%M%S")
-    command = local("tar -cvzf versions/web_static_{}.tgz ./web_static".format(date_time))
+    command = local("tar -cvzf versions/web_static_{}.tgz ./web_stat\
+ic".format(date_time))
 
     if command.succeeded:
         size = getsize('versions/web_static_{}.tgz'.format(date_time))
-        print("web_static packed: versions/web_static_{}.tgz -> {}Bytes".format(date_time, size))
+        print("web_static packed: versions/web_static_{}.tgz -> {}Byt\
+es".format(date_time, size))
         return ('versions/web_static_{}.tgz'.format(date_time))
     else:
         return None
