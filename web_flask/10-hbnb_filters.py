72e843f8d99868970dc324a3e0a4b07959629202
Yulieth <yulyzulu>
2020-04-22 19:02:26 -0500

    Update files

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -13,14 +13,10 @@ def delete_SQLA(self):
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
+@app.route('/hbnb_filters', strict_slashes=False)
 def display_HTML(id=None):
     states = storage.all('State')
-    for state in states.values():
-        if state.id == id:
-            states = state
-    return render_template('9-states.html', states=states, id=id)
+    return render_template('10-hbnb_filters.html', states=states)
 
 
 if __name__ == '__main__':
